bdf2590d08c234e23ba1084273d4284ea30911dc
Ayush Garg <62711517+ayush4921@users.noreply.github.com>
2021-07-01 10:12:18 +0800

    Fix

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ import pandas as pd
 import os
 from flask import Flask, request, render_template
 dictionary = PyDictionary()
-
-
 app = Flask(__name__)
 
 
@@ -59,20 +57,3 @@ def handle_website():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-'''
-Variable    Description
-loop.index  The current iteration of the loop. (1 indexed)
-loop.index0 The current iteration of the loop. (0 indexed)
-loop.revindex   The number of iterations from the end of the loop (1 indexed)
-loop.revindex0  The number of iterations from the end of the loop (0 indexed)
-loop.first  True if first iteration.
-loop.last   True if last iteration.
-loop.length The number of items in the sequence.
-loop.cycle  A helper function to cycle between a list of sequences. See the explanation below.
-loop.depth  Indicates how deep in a recursive loop the rendering currently is. Starts at level 1
-loop.depth0 Indicates how deep in a recursive loop the rendering currently is. Starts at level 0
-loop.previtem   The item from the previous iteration of the loop. Undefined during the first iteration.
-loop.nextitem   The item from the following iteration of the loop. Undefined during the last iteration.
-loop.changed(*val)  True if previously called with a different value (or not called at all).
-'''
